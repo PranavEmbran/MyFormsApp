8129242ba2b510ffa887033fedfb264493aee7db
Pranav Embran S <pranavembrans@gmail.com>
2025-05-27 17:42:10 +0530

    26/05 c1

---
+++ ClientServerDB_App/ClientServerPing_DB_App/Client.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientServerPing_DB_App
+{
+    internal class Client
+    {
+        public static void main(string[] args)
+        {
+
+        }
+    }
+}
